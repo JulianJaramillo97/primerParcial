d4ee90a18d7a90a676ddda6c5b253cb270c1489b
JulianJaramillo <julian_jaramillo69212@elpoli.edu.co>
2023-10-06 22:13:09 -0500

    parcial

---
+++ punto4/solucion.js
@@ -0,0 +1,7 @@
+let edad = prompt("escriba su edad");
+
+if(edad >= 18){
+    document.write("Usted es mayor de edad");
+}else{
+    document.write("Usted es menor de edad");
+}
---
+++ punto5/solucion.js
@@ -0,0 +1,5 @@
+let peso = prompt("ingrese su peso");
+let altura = prompt("Ingrese su altura");
+let IMC = (altura*altura)/peso;
+
+alert("su indice de masa muscular es: "+ IMC);
